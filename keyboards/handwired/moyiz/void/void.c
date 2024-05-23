e49b3fbcd9424deeee9ab0738012efea4f9d0a72
moyiz <8603313+moyiz@users.noreply.github.com>
2024-05-23 11:39:04 +0300

    void: qmk lint

--- keyboards/handwired/moyiz/void/void.c
+++ keyboards/handwired/moyiz/void/void.c
@@ -16,6 +16,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "void.h"
 
+// clang-format off
+#define LAYOUT( \
+    L00, L01, L02, L03, L04, L05,              R05, R04, R03, R02, R01, R00, \
+    L10, L11, L12, L13, L14, L15,              R15, R14, R13, R12, R11, R10, \
+    L20, L21, L22, L23, L24, L25, L26,    R26, R25, R24, R23, R22, R21, R20, \
+                   L33, L34, L35, L36,    R36, R35, R34, R33 \
+) { \
+    { L00, L01, L02, L03, L04, L05, ___ }, \
+    { L10, L11, L12, L13, L14, L15, ___ }, \
+    { L20, L21, L22, L23, L24, L25, L26 }, \
+    { ___, ___, ___, L33, L34, L35, L36 }, \
+    { R00, R01, R02, R03, R04, R05, ___ }, \
+    { R10, R11, R12, R13, R14, R15, ___ }, \
+    { R20, R21, R22, R23, R24, R25, R26 }, \
+    { ___, ___, ___, R33, R34, R35, R36 } \
+}
+
+// clang-format on
 const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
     {{0, 4}, {1, 4}, {2, 4}, {3, 4}, {4, 4}, {5, 4}, {6, 4}},
     {{0, 5}, {1, 5}, {2, 5}, {3, 5}, {4, 5}, {5, 5}, {6, 5}},
--- keyboards/handwired/moyiz/void/void.h
+++ keyboards/handwired/moyiz/void/void.h
@@ -20,19 +20,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "quantum.h"
 
 #define ___ KC_NO
-
-#define LAYOUT( \
-    L00, L01, L02, L03, L04, L05,              R05, R04, R03, R02, R01, R00, \
-    L10, L11, L12, L13, L14, L15,              R15, R14, R13, R12, R11, R10, \
-    L20, L21, L22, L23, L24, L25, L26,    R26, R25, R24, R23, R22, R21, R20, \
-                   L33, L34, L35, L36,    R36, R35, R34, R33 \
-) { \
-    { L00, L01, L02, L03, L04, L05, ___ }, \
-    { L10, L11, L12, L13, L14, L15, ___ }, \
-    { L20, L21, L22, L23, L24, L25, L26 }, \
-    { ___, ___, ___, L33, L34, L35, L36 }, \
-    { R00, R01, R02, R03, R04, R05, ___ }, \
-    { R10, R11, R12, R13, R14, R15, ___ }, \
-    { R20, R21, R22, R23, R24, R25, R26 }, \
-    { ___, ___, ___, R33, R34, R35, R36 } \
-}
