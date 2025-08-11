4b5b23c149a0e8ab45a1e2dfa491206cf8c1ab1b
moyiz <8603313+moyiz@users.noreply.github.com>
2025-08-12 00:06:54 +0300

    silakka54: thumb buttons for mouse layer

--- keyboards/silakka54/keymaps/moyiz/keymap.c
+++ keyboards/silakka54/keymaps/moyiz/keymap.c
@@ -20,19 +20,12 @@
 
 enum layer {
     _QW = 0, // Qwerty
-    _EN,     // Engram
     _IS,     // ISRT
-    _MT,     // MTGAP
-    _AP,     // APTv3
     _LOW,    // Lower
     _RAI,    // Raise
     _ADJ,    // Adjust
     _MS,     // Mouse
     _GM,     // Gaming
-    // _WM,     // Window Manager
-    _SS, // System
-    // _MC,     // Misc
-    // _LK,     // Lock
 };
 
 // clang-format off
@@ -94,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______,                   _______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______,
         _______, _______, _______, _______, _______, _______,                   _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,
         _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, _______,                   _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______,
-                                            _______, _______, _______, _______, _______, _______
+                                            _______, _______, _______, KC_BTN1, KC_BTN3, KC_BTN2
     ),
 
     // Gaming
@@ -103,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                      _______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______,
         _______, KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                      _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,
         _______, KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,                      _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______,
-                                            _______, _______, KC_SPC,  _______, _______, _______
+                                            _______, _______, KC_SPC,  KC_BTN1, KC_BTN3, KC_BTN2
     ),
 
 };
